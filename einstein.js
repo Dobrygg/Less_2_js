77c898aba441110490b975c54d1f7264ee73e2b0
dobrygg <Dobryg4@mail.ru>
2024-10-07 13:48:35 +0300

    переделал

---
+++ einstein.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myWeight = 85;
+
+const speedLight = 3e8;
+
+console.log('Энергия тела ' + myWeight * (speedLight ** 2));
--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 'use strict';
 
 
-const product = 'plate';
-const quantityProduct = '5';
-const category = 'kitchen utensils';
-const price = '200';
+// const product = 'plate';
+// const quantityProduct = '5';
+// const category = 'kitchen utensils';
+// const price = '200';
 
-console.log(product);
-console.log(quantityProduct * price +  " Цена заказа");
+// console.log(product);
+// console.log(quantityProduct * price +  " Цена заказа");
+
+const product = prompt('Наименование товара');
+const quantityProduct = prompt('Количество товара');
+const category = prompt('Категория товара');
+const price = prompt('Цена товара');
+
+
+
+
+console.log(typeof +product);
+console.log(typeof +quantityProduct);
+console.log(typeof +category);
+console.log(typeof +price);
+
+
+console.log('На складе ' + quantityProduct + ' единицы товара ' + product + ' на сумму ' + price + ' рублей');
