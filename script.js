40c621a8ed59357f86ed10947d58803c6369958b
dobrygg <Dobryg4@mail.ru>
2024-10-08 14:17:49 +0300

    переделалпше фвв ю

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 'use strict';
 
 const product = prompt('Наименование товара');
-const quantityProduct = prompt('Количество товара');
+const quantityProduct = +prompt('Количество товара');
 const category = prompt('Категория товара');
-const price = prompt('Цена товара');
+const price = +prompt('Цена товара');
+
+console.log('На складе ' + quantityProduct + ' единицы товара ' + product + ' на сумму ' + price * +quantityProduct + ' рублей');
 
 console.log(typeof product);
 console.log(typeof quantityProduct);
 console.log(typeof category);
 console.log(typeof price);
 
-const quantityProductNumber = Number.parseFloat(quantityProduct);
-const priceNumber = Number.parseFloat(price);
 
-console.log('На складе ' + quantityProductNumber + ' единицы товара ' + product + ' на сумму ' + priceNumber + ' рублей');
 
 
