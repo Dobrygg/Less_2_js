1ae6204a13be6fee99c42f39c52a63c6c90e354c
dobrygg <Dobryg4@mail.ru>
2024-10-10 22:33:27 +0300

    убрал комментирование

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// 'use strict';
+'use strict';
 
-// const product = prompt('Наименование товара');
-// const quantityProduct = +prompt('Количество товара');
-// const category = prompt('Категория товара');
-// const price = +prompt('Цена товара');
+const product = prompt('Наименование товара');
+const quantityProduct = +prompt('Количество товара');
+const category = prompt('Категория товара');
+const price = +prompt('Цена товара');
 
-// if (Number.isFinite(quantityProduct)) {
-//     } else {
-//         console.log ('Вы ввели некорректные данные');
-//     }
+if (Number.isFinite(quantityProduct)) {
+    } else {
+        console.log ('Вы ввели некорректные данные');
+    }
 
-// if (Number.isFinite(price)) {
-// } else {
-//     console.log ('Вы ввели некорректные данные');
-// }    
+if (Number.isFinite(price)) {
+} else {
+    console.log ('Вы ввели некорректные данные');
+}    
 
 
 
--- tasks.js
+++ tasks.js
@@ -1,29 +1,29 @@
-// 'use strict';
+'use strict';
 
-// const rain = Math.round(Math.random());
+const rain = Math.round(Math.random());
 
-// if (rain === 1){
-//     alert('Пошёл дождь. Возьмите зонт!')}
-//     else {
-//         alert('Дождя нет!')
-// }
+if (rain === 1){
+    alert('Пошёл дождь. Возьмите зонт!')}
+    else {
+        alert('Дождя нет!')
+}
 
-// let mathematics = +prompt('Введите кол-во баллов по математике:');
-// let language = +prompt('Введите кол-во баллов по русскому языку:');
-// let informatics = +prompt('Введите кол-во баллов по информатике:');
+let mathematics = +prompt('Введите кол-во баллов по математике:');
+let language = +prompt('Введите кол-во баллов по русскому языку:');
+let informatics = +prompt('Введите кол-во баллов по информатике:');
 
-// if (mathematics + language + informatics >= 265) {
-//     alert('Поздравляю, вы поступили на бюджет!')
-// } else {
-//     alert('Вы не поступили на бюджет!')
-// }
+if (mathematics + language + informatics >= 265) {
+    alert('Поздравляю, вы поступили на бюджет!')
+} else {
+    alert('Вы не поступили на бюджет!')
+}
 
-// let money = +prompt('Сколько денег вы хотите снять?');
-// if (money <= 99) {
-//     console.log('Вы не можете снять сумму меньше 100 рублей')
-// } else if (money >= 100) {
-//     console.log((Math.floor(money / 100) * 100) + ' Вы можете снять наличными')
-// }
+let money = +prompt('Сколько денег вы хотите снять?');
+if (money <= 99) {
+    console.log('Вы не можете снять сумму меньше 100 рублей')
+} else if (money >= 100) {
+    console.log((Math.floor(money / 100) * 100) + ' Вы можете снять наличными')
+}
 
 
 
