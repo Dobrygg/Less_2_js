2919a06a91ac8a9313e5594cd56bdc654fed4717
dobrygg <Dobryg4@mail.ru>
2024-10-16 13:59:03 +0300

    16 10

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ const quantityProduct = +prompt('Количество товара');
 const category = prompt('Категория товара');
 const price = +prompt('Цена товара');
 
-if (quantityProduct / 1 === quantityProduct && price / 1 === price) {
-} else {
+if (isNaN(quantityProduct ) || isNaN(price)) {
     console.log ('Вы ввели некорректные данные');
+} else {
+    
 }
 
 
--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,17 +1,23 @@
 'use strict';
 
 const money = +prompt('Сколько вы зарабатываете');
-let moneyRemains = money - 15000;
-let moneyPercentMax = 8950;
+let rest = money;
+let tax = 0;
 
-if (money <= 15000) {
-    console.log(`Ваш подоходный налог = ${(money * 0.13)}`);
+if (rest > 50000) {
+    rest = money - 50000
+    tax = (rest * 0.3) + ((money - 15000 - rest) * 0.2) + ((money - 35000 - rest) * 0.13)
+    console.log(`Ваш подоходный налог = ${tax}`);
 } 
 
-if (money <= 50000) {
-    console.log(`Ваш подоходный налог = ${((money - moneyRemains) * 0.13) + (moneyRemains * 0.2)}`);
+if (rest > 15000) {
+    rest = money - 15000
+    tax = (rest * 0.2) + ((money - rest)* 0.13) 
+    console.log(`Ваш подоходный налог = ${tax}`);
 }
 
-if (money > 50000) {
-    console.log(`Ваш подоходный налог = ${(moneyPercentMax + ((money - 50000) * 0.3))}`);
+if (rest > 0) {
+    tax = rest * 0.13
+    console.log(`Ваш подоходный налог = ${tax}`);
 }
+
