1c1b4952ed5efa8046bd5c08244188acad87bbf7
dobrygg <Dobryg4@mail.ru>
2024-10-21 14:41:53 +0300

    убрал пробел

--- Task02.js
+++ Task02.js
@@ -7,4 +7,4 @@ function createWords(word) {
     return wordsTrim.replace(/^./, wordsTrim[0].toUpperCase(), wordsTrim[1].toLowerCase);
 }
 
-console.log(createWords( word)); // Функция с заданными параметрами.
+console.log(createWords(word)); // Функция с заданными параметрами.
