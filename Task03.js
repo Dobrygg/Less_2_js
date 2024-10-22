edba7565a0f4c1ef130fd6369e1714a069fad32c
dobrygg <Dobryg4@mail.ru>
2024-10-22 22:44:01 +0300

    переделал 3 задание

--- Task03.js
+++ Task03.js
@@ -1,13 +1,24 @@
 'use strict';
+let to;
+let tot;
+let tota;
+
+
 
 function calculate(total, product, promoCode) {
     if (product > 10) {
         total = total - (total * 0.03);
     }
-    if (total > 30000) {
-        total = total  - ((total - 30000) * 0.15);
+    if (total > 30000 && product > 10) {
+        total = (total - (total * 0.03))  - ((total - 30000) * 0.15);
+    } else if (total > 30000) {
+        total = total - (total - 30000) * 0.15;
     }
-    if (promoCode === "METHED") {
+    if (promoCode === "METHED" && product > 10 && total > 30000) {
+        total = (total - (total * 0.03)) - ((total - 30000) * 0.15) - (total - (total * 0.1));
+    } else if (product > 10 && total > 30000) {
+        total = (total - (total * 0.03))  - ((total - 30000) * 0.15); 
+    }  else if(promoCode === "METHED") {
         total = total - (total * 0.1);
     }
     if (promoCode === "G3H2Z1" && total > 2000) {
