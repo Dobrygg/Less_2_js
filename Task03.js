15e5df5e965c2434fe3b21bf3a7f0a3e330e1667
dobrygg <Dobryg4@mail.ru>
2024-10-23 22:33:39 +0300

    2

--- Task03.js
+++ Task03.js
@@ -5,11 +5,12 @@ const word = '  привЕт МИр';
 function createWords(word) {
     const wordsTrim = word.trim();
     const line = wordsTrim[0].toUpperCase() + wordsTrim.slice(1).toLowerCase();
-    let lineRev = "";
+    let lineRev = '';
     for (let i = line.length; i >= 0; i -= 1) {
         lineRev += line[i];
     }
     return lineRev;
+
 }
 
 console.log(createWords(word));
