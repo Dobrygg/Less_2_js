84730db23267a3ce57111d74813f952011160e5a
dobrygg <Dobryg4@mail.ru>
2024-11-09 14:48:24 +0300

    второй вариант

--- game01.js
+++ game01.js
@@ -5,16 +5,24 @@ const runNum = Math.floor((Math.random() * 100) + 1);
 function game(runNum) {
     let playerNum;
     do { 
-    playerNum = prompt('Введите число');
-    if (isNaN(playerNum)) {
+        playerNum = prompt('Введите число');
+        if (playerNum === null) {
+            alert("Игра завершена.");
+            break;
+        } else if (isNaN(playerNum)) {
             alert('Введите число')
         } else if (runNum > playerNum) {
             alert('Загаданное число больше вашего')
         } else if (runNum < playerNum) {
             alert('Загаданное число меньше вашего вашего')
+        } else if (playerNum === null) {
+            alert("Игра завершена.");
+            break;
         }
+        
     } while (runNum !== Number(playerNum)); 
     
+    alert(`Правильно`)
     return runNum;   
 
 }
