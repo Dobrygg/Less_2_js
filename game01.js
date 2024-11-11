1cdaf306219470b871e4549dcdb7b08f3c24aaad
dobrygg <Dobryg4@mail.ru>
2024-11-11 15:32:58 +0300

    переделал 11 11

--- game01.js
+++ game01.js
@@ -6,25 +6,26 @@ function game(runNum) {
     let playerNum;
     do { 
         playerNum = prompt('Введите число');
-        if (playerNum === null) {
-            alert("Игра завершена.");
-            break;
-        } else if (isNaN(playerNum)) {
-            alert('Введите число')
-        } else if (runNum > playerNum) {
-            alert('Загаданное число больше вашего')
-        } else if (runNum < playerNum) {
-            alert('Загаданное число меньше вашего вашего')
-        } else if (playerNum === null) {
-            alert("Игра завершена.");
-            break;
+
+        switch (runNum !== playerNum) {
+            case playerNum === null:
+                alert("Игра завершена.");
+                return; 
+            case isNaN(playerNum):
+                alert('Введите число');
+                break;
+            case runNum > playerNum:
+                alert('Загаданное число больше вашего');
+                break;
+            case runNum < playerNum:
+                alert('Загаданное число меньше вашего');
+                break;
+            case runNum === Number(playerNum):
+                alert(`Правильно`);
+                return runNum; 
         }
         
-    } while (runNum !== Number(playerNum)); 
-    
-    alert(`Правильно`)
-    return runNum;   
-
+    } while (runNum !== playerNum); 
 }
 
 console.log(game(runNum));
