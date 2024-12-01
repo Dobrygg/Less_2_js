010f086a674877a561f9ede0cd9efbae1fd786a2
dobrygg <Dobryg4@mail.ru>
2024-12-01 14:55:09 +0300

    1 12

--- Task01_2.js
+++ Task01_2.js
@@ -1,14 +1,25 @@
 'use strict'
 
 const arr = [1, 2, 3, 4, 5, 6];
+
 function creatArr(arr) {
-    const randomNum = Math.floor((Math.random() * 10) + 1);
-    arr.push(randomNum);
-    const sumNumber = arr.reduce(function (sum, num) {
+    const newArr = [...arr];
+    const sumNumber = newArr.reduce(function (sum, num) {
         return sum + num
     }, 0);
-    if (sumNumber < 50) {
-        creatArr(arr)
-    };
-    return arr;
+    console.log(sumNumber)
+    {
+        if (sumNumber < 50) {
+            const randomNum = Math.floor((Math.random() * 10) + 1);
+            newArr.push(randomNum);
+            console.log(newArr);
+            
+        };
+        creatArr();
+    }
+    
+    
+    // return newArr;
 };
+
+// creatArr(arr);
--- game01.js
+++ game01.js
@@ -1,13 +1,13 @@
 'use strict'
 
 function game() {
-    const ranNum = Math.floor((Math.random() * 10) + 1);
-    let answer = prompt('Введите число от 1 до 100'); 
-    if (ranNum !== +answer && answer !== null) {
+    const ranNum = 11;
+    const answer = prompt('Введите число от 1 до 100'); 
+    {
         switch (true) {
-            case answer === null:
-                alert("Игра завершена.");
-                break;
+            case ranNum === +answer:
+                alert(`Правильно! ${ranNum}`);
+                return;
             case isNaN(answer):
             case +answer > 100:
             case +answer < 1:
@@ -19,13 +19,46 @@ function game() {
             case ranNum < answer:
                 alert('Загаданное число меньше вашего');
                 break;
-            };
-            game (); 
+            case answer === null:
+                alert("Игра завершена.");
+                return; 
         }
-    if (ranNum === +answer) {
-        alert(`Правильно ${ranNum}`);
-    } 
+            game (); 
+        
+        } 
+        
     return ranNum;
 };
 
 game();
+
+
+// function game() {
+//     const ranNum = Math.floor((Math.random() * 10) + 1);
+//     const answer = prompt('Введите число от 1 до 100'); 
+//     if (ranNum !== +answer && answer !== null) {
+//         switch (true) {
+//             case answer === null:
+//                 alert("Игра завершена.");
+//                 break;
+//             case isNaN(answer):
+//             case +answer > 100:
+//             case +answer < 1:
+//                 alert('Нужно ввести число от 1 до 100');
+//                 break;
+//             case ranNum > answer:
+//                 alert('Загаданное число больше вашего');
+//                 break;
+//             case ranNum < answer:
+//                 alert('Загаданное число меньше вашего');
+//                 break;
+//             };
+//             game (); 
+//         }
+//     if (ranNum === +answer) {
+//         alert(`Правильно ${ranNum}`);
+//     } 
+//     return ranNum;
+// };
+
+// game();
