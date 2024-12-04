9578e3f9662845fbae6fd1d88cc1a535a88f5ad8
dobrygg <Dobryg4@mail.ru>
2024-12-04 14:26:28 +0300

    4 12

--- Task2_02.js
+++ Task2_02.js
@@ -1,25 +1,24 @@
 'use strict';
 
 const cart = {
-  METHED: 'METHED',
-  NEWYEAR:'NEWYEAR',
   items: [],
   count: 0,
-  totalPrice: 0,
   discount: 0,
-
+  METHED: 'METHED',
+  NEWYEAR:'NEWYEAR',
   calculateltemPrice() {
-    this.items.reduce((sum, item) =>
+    let summer = this.items.reduce((sum, item) =>
       sum + item.price * item.quantity, 0);
-
+    summer -= (summer * this.discount) / 100;
+    return summer;
   },
 
-  getTotalPrice() {
-    let total = this.items.reduce((sum, item) =>
-      sum + item.price * item.quantity, 0);
-    total -= (total * this.discount) / 100;
-    return total;
-  },
+  // get totalPrice() {
+  //   let summer = this.items.reduce((sum, item) =>
+  //     sum + item.price * item.quantity, 0);
+      
+  //   return summer;
+  // },
 
   add(name, price, quantity = 1) {
     const newItems = {
@@ -36,38 +35,134 @@ const cart = {
   },
 
   clear() {
-    this.items = [];
-    this.totalPrice = 0;
+    this.items = [],
+    this.discount = 0,
     this.count = 0;
-    this.discount = 0;
   },
 
   print() {
     console.log(JSON.stringify(this.items));
   },
 
-  setDiscount(promocode) {
+  _discount(promocode) {
     this.discount = 0;
     if (promocode === this.METHED) {
-        this.discount = 15
+      this.discount = 15;
     };
     if (promocode === this.NEWYEAR){
-        this.discount = 21
+      this.discount = 21;
     };
   }
 };
 
-cart.add('banan', 5, 0);
-cart.add('mango', 4, 0);
-cart.add('qiwi', 1, 100);
+cart.add('banan', 5, 2);
+cart.add('mango', 4, 1);
+cart.add('qiwi', 1, 10);
+cart.print();
+
 console.log(cart.calculateltemPrice());
-console.log(cart.print());
-console.log(cart.getTotalPrice());
-cart.setDiscount(cart.METHED);
-console.log(cart.getTotalPrice());
-console.log(cart.print());
-cart.total = 100;
-console.log(cart.getTotalPrice());
-console.log(cart.print());
+cart._discount(cart.METHED);
+console.log(cart.calculateltemPrice());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cart = {
+//   METHED: 'METHED',
+//   NEWYEAR:'NEWYEAR',
+//   items: [],
+//   count: 0,
+//   discount: 0,
+
+//   calculateltemPrice() {
+//     let total = this.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+//     total -= (total * this._discount) / 100;
+//     // this.setTotalPrice(total);
+//     },
+
+// //     setTotalPrice(value){
+// //       this._totalPrice = value;
+// // }, 
+//   get totalPrice(){
+//     return this.items;
+    
+//   },
+
+//   // getTotalPrice() {
+//   //   let total = this.items.reduce((sum, item) =>
+//   //     sum + item.price * item.quantity, 0);
+//   //   total -= (total * this.discount) / 100;
+//   //   return total;
+//   // },
+
+//   add(name, price, quantity = 1) {
+//     const newItems = {
+//       name,
+//       price,
+//       quantity,
+//     };
+//     this.items.push(newItems);
+//     this.increaseCount(quantity);
+//   },
+
+//   increaseCount(num) {
+//     this.count += num;
+//   },
+
+//   clear() {
+//     this.items = [];
+//     // this.totalPrice = 0;
+//     this.count = 0;
+//     this.discount = 0;
+//   },
+
+//   print() {
+//     console.log(JSON.stringify(this.items));
+//   },
+
+//   _discount(promocode) {
+//     this.discount = 0;
+//     if (promocode === this.METHED) {
+//       this._totalPrice -= (this._totalPrice * 15) / 100;
+//     };
+//     if (promocode === this.NEWYEAR){
+//       this._totalPrice -= (this._totalPrice * 21) / 100;
+//     };
+//   }
+// };
+
+// cart.add('banan', 5, 2);
+// cart.add('mango', 4, 1);
+// cart.add('qiwi', 1, 100);
+// console.log(cart.calculateltemPrice());
+// cart.print();
+// console.log(cart.calculateltemPrice());
+// console.log(cart.print());
+// console.log(cart.totalPrice());
+// cart._discount(cart.METHED);
+// console.log(cart.totalPrice());
+// console.log(cart.print());
+// cart.total = 100;
+// console.log(cart.totalPrice());
+// console.log(cart.calculateltemPrice());
+// console.log(cart.print());
 
 
